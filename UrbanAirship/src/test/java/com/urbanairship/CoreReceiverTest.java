b1361e404ea57b0d13f8567063007ea54122d0a4
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2015-02-12 11:46:07 -0800

    [MB-873] Clear the pending IAN on notification open

---
+++ UrbanAirship/src/test/java/com/urbanairship/CoreReceiverTest.java
@@ -0,0 +1,114 @@
+/*
+Copyright 2009-2015 Urban Airship Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.urbanairship;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.urbanairship.push.PushManager;
+import com.urbanairship.push.PushMessage;
+import com.urbanairship.push.ian.InAppManager;
+import com.urbanairship.push.ian.InAppNotification;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class CoreReceiverTest {
+
+    Context context;
+
+    @Before
+    public void before() {
+        context = mock(Context.class);
+        when(context.getSystemService(Context.NOTIFICATION_SERVICE)).thenReturn(mock(NotificationManager.class));
+    }
+    /**
+     * Test when a notification is opened it clears the pending in app notification.
+     */
+    @Test
+    public void testOpenProxyClearPendingInAppNotification() {
+        InAppNotification inAppNotification = new InAppNotification.Builder()
+                .setAlert("oh hi")
+                .setExpiry(1000l)
+                .create();
+
+        // Set the pending IAN
+        InAppManager inAppManager = UAirship.shared().getInAppManager();
+        inAppManager.setPendingNotification(inAppNotification);
+
+        // Create the push message with the IAN
+        Bundle pushBundle = new Bundle();
+        pushBundle.putString(PushMessage.EXTRA_IN_APP_NOTIFICATION, inAppNotification.toJsonValue().toString());
+        PushMessage message = new PushMessage(pushBundle);
+
+        // Create the intent
+        Intent intent = new Intent().putExtra(PushManager.EXTRA_PUSH_MESSAGE, message);
+
+        // Call the proxy
+        CoreReceiver.handleNotificationOpenedProxy(context, intent);
+
+        assertNull(inAppManager.getPendingNotification());
+    }
+
+
+    /**
+     * Test when a notification is opened it clears the pending in app notification.
+     */
+    @Test
+    public void testButtonClickProxyClearPendingInAppNotification() {
+        InAppNotification inAppNotification = new InAppNotification.Builder()
+                .setAlert("oh hi")
+                .setExpiry(1000l)
+                .create();
+
+        // Set the pending IAN
+        InAppManager inAppManager = UAirship.shared().getInAppManager();
+        inAppManager.setPendingNotification(inAppNotification);
+
+        // Create the push message with the IAN
+        Bundle pushBundle = new Bundle();
+        pushBundle.putString(PushMessage.EXTRA_IN_APP_NOTIFICATION, inAppNotification.toJsonValue().toString());
+        PushMessage message = new PushMessage(pushBundle);
+
+        // Create the intent
+        Intent intent = new Intent()
+                .putExtra(PushManager.EXTRA_PUSH_MESSAGE, message)
+                .putExtra(PushManager.EXTRA_NOTIFICATION_BUTTON_ID, "button id");
+
+        // Call the proxy
+        CoreReceiver.handleNotificationButtonOpenedProxy(context, intent);
+
+        assertNull(inAppManager.getPendingNotification());
+    }
+}
