79d08a1dc893e175344074f778e8a55988609f82
hcrowell <helen.crowell@urbanairship.com>
2015-03-05 15:12:28 -0800

    [MB-980] Moved launcher icons into mipmap folders as recommended.

--- tools/set-android-icons.sh
+++ tools/set-android-icons.sh
@@ -5,6 +5,8 @@ p=`dirname "$0"`
 f="${p}/../samples/${2}/res"
 echo ${f}
 
-sips --resampleWidth 48 "${1}" --out "${f}/drawable-mdpi/ic_launcher.png"
-sips --resampleWidth 72 "${1}" --out "${f}/drawable-hdpi/ic_launcher.png"
-sips --resampleWidth 96 "${1}" --out "${f}/drawable-xhdpi/ic_launcher.png"
+sips --resampleWidth 48 "${1}" --out "${f}/mipmap-mdpi/ic_launcher.png"
+sips --resampleWidth 72 "${1}" --out "${f}/mipmap-hdpi/ic_launcher.png"
+sips --resampleWidth 96 "${1}" --out "${f}/mipmap-xhdpi/ic_launcher.png"
+sips --resampleWidth 144 "${1}" --out "${f}/mipmap-xxhdpi/ic_launcher.png"
+sips --resampleWidth 192 "${1}" --out "${f}/mipmap-xxxhdpi/ic_launcher.png"
