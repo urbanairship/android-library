e5d30b8f5c9ffc75f56c9ff04038192c6dc61469
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2015-06-12 13:27:14 -0700

    Fix PushManager build

--- UrbanAirship/src/main/java/com/urbanairship/push/PushManager.java
+++ UrbanAirship/src/main/java/com/urbanairship/push/PushManager.java
@@ -29,6 +29,7 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
