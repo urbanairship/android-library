9c43ab3bcb187c06cc1302e177faea5689327f9d
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-04-13 11:14:47 -0700

    [MB-1734] Fix GcmPushReceiver logging erroneous errors (#815)

--- urbanairship-sdk/src/main/java/com/urbanairship/push/GcmPushReceiver.java
+++ urbanairship-sdk/src/main/java/com/urbanairship/push/GcmPushReceiver.java
@@ -25,48 +25,110 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package com.urbanairship.push;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.content.pm.ServiceInfo;
+import android.os.Build;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Base64;
 
-import com.google.android.gms.gcm.GcmReceiver;
 import com.urbanairship.Autopilot;
 import com.urbanairship.Logger;
-import com.urbanairship.UAirship;
 
 /**
  * WakefulBroadcastReceiver that receives GCM messages and delivers them to both the application-specific GcmListenerService subclass,
  * and Urban Airship's PushService.
  */
-public class GcmPushReceiver extends GcmReceiver {
+public class GcmPushReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Autopilot.automaticTakeOff(context);
 
-        // CE-1574: Security exception is sometimes thrown when the GcmReceiver's onReceive tries to start
-        // the instance ID service
+        Logger.verbose("GcmPushReceiver - Received intent: " + intent.getAction());
+
+        normalizeIntent(context, intent);
+
+        if (intent.getAction() == null) {
+            if (this.isOrderedBroadcast()) {
+                this.setResultCode(Activity.RESULT_CANCELED);
+            }
+            return;
+        }
+
+        // Send the intent to the InstanceIdService or the GcmIntentService
         try {
-            super.onReceive(context, intent);
+            ComponentName componentName = startWakefulService(context, intent);
+            if (isOrderedBroadcast()) {
+                setResultCode(componentName == null ? 404 : Activity.RESULT_OK);
+            }
         } catch (SecurityException e) {
-            Logger.error("Received security exception from GcmReceiver: ", e);
-
-            if (!GcmConstants.ACTION_GCM_RECEIVE.equals(intent.getAction())) {
-                // Trying to do any further registrations with GCM leads to a SecurityException - bad process.
-                // Lets assume the token was trying to be refreshed, so lets clear the token to force
-                // it to be regenerated on next app start and hope GCM is in a better spot.
-                UAirship.shared().getPushManager().setGcmToken(null);
-
-                return;
+            Logger.error("GcmPushReceiver - Error while delivering the message to the serviceIntent", e);
+            if (this.isOrderedBroadcast()) {
+                this.setResultCode(401);
             }
         }
 
-        Logger.verbose("GcmPushReceiver - Received intent: " + intent.getAction());
+        // Forward any GCM Receive messages to Push Service
         if (GcmConstants.ACTION_GCM_RECEIVE.equals(intent.getAction())) {
             Intent pushIntent = new Intent(context, PushService.class)
                     .setAction(PushService.ACTION_RECEIVE_GCM_MESSAGE)
                     .putExtra(PushService.EXTRA_INTENT, intent);
 
             startWakefulService(context, pushIntent);
+
+            if (this.isOrderedBroadcast()) {
+                this.setResultCode(Activity.RESULT_OK);
+            }
+        }
+    }
+
+    /**
+     * Normalizes the intent based on the GcmReceiver logic.
+     * @param context The application context.
+     * @param intent The intent.
+     */
+    private void normalizeIntent(Context context, Intent intent) {
+        // Clear the component
+        intent.setComponent(null);
+
+        // Set the package name
+        intent.setPackage(context.getPackageName());
+
+        // Remove the category on pre kitkat devices
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            intent.removeCategory(context.getPackageName());
+        }
+
+        // Decode the gcm data if its base 64 encoded
+        String encodedData = intent.getStringExtra("gcm.rawData64");
+        if (encodedData != null) {
+            intent.putExtra("rawData", Base64.decode(encodedData, 0));
+            intent.removeExtra("gcm.rawData64");
+        }
+
+        // Registration, iid, and refresh needs to start the InstanceID service.
+        String from = intent.getStringExtra("from");
+        if ("com.google.android.c2dm.intent.REGISTRATION".equals(intent.getAction()) || "google.com/iid".equals(from) || "gcm.googleapis.com/refresh".equals(from)) {
+            intent.setAction("com.google.android.gms.iid.InstanceID");
+        }
+
+        // Try to set the service class name
+        ResolveInfo resolveInfo = context.getPackageManager().resolveService(intent, 0);
+        if (resolveInfo != null && resolveInfo.serviceInfo != null) {
+            ServiceInfo serviceInfo = resolveInfo.serviceInfo;
+            if (context.getPackageName().equals(serviceInfo.packageName) && serviceInfo.name != null) {
+                String serviceName = serviceInfo.name;
+                serviceName = serviceName.startsWith(".") ? context.getPackageName() + serviceName : serviceName;
+
+                Logger.debug("GcmPushReceiver - Forwarding GCM intent to " + serviceName);
+                intent.setClassName(context.getPackageName(), serviceName);
+            } else {
+                Logger.error("GcmPushReceiver - Error resolving target intent service, skipping classname enforcement. Resolved service was: " + serviceInfo.packageName + "/" + serviceInfo.name);
+            }
         }
     }
 }
