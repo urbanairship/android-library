8c9b0be7e22a7d28ee7d6ad63351019adacb442e
Devin Smythe <devin.smythe@urbanairship.com>
2017-07-18 10:19:46 -0700

    [MB-2309] Added close calls for some cursors in AutomationDataManager. (#211)
    * Added close calls for some cursors in AutomationDataManager
    
    * Forgot a close statement
    
    * [MB-2309] Enable strict mode in sample application
    
    * [MB-2309] Add null check

---
+++ sample/src/main/java/com/urbanairship/sample/SampleApplication.java
@@ -0,0 +1,31 @@
+/* Copyright 2017 Urban Airship and Contributors */
+
+package com.urbanairship.sample;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+
+public class SampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+    }
+}
