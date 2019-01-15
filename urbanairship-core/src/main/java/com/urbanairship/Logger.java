c90db95803fcdc7956c7b10f5942159f5d4e3c7f
Marc Sciglimpaglia <marc@urbanairship.com>
2019-01-15 14:55:58 -0800

    [MOBILE-439] logger listener

--- urbanairship-core/src/main/java/com/urbanairship/Logger.java
+++ urbanairship-core/src/main/java/com/urbanairship/Logger.java
@@ -8,6 +8,7 @@ import android.util.Log;
 
 import com.urbanairship.util.UAStringUtil;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 /**
@@ -31,6 +32,11 @@ public class Logger {
      * Defaults to <code>android.util.Log.ERROR</code>.
      */
     private static int logLevel = Log.ERROR;
+
+    /**
+     * A list of listeners.
+     */
+    private static ArrayList<LoggerListener> listeners = new ArrayList<>();
 
     /**
      * Private, unused constructor
@@ -61,6 +67,28 @@ public class Logger {
      */
     public static int getLogLevel() {
         return logLevel;
+    }
+
+    /**
+     * Adds a listener.
+     *
+     * @param listener The listener.
+     */
+    public static void addListener(@NonNull LoggerListener listener) {
+        synchronized (listeners) {
+            listeners.add(listener);
+        }
+    }
+
+    /**
+     * Removes a listener.
+     *
+     * @param listener The listener.
+     */
+    public static void removeListener(@NonNull LoggerListener listener) {
+        synchronized (listeners) {
+            listeners.remove(listener);
+        }
     }
 
     /**
@@ -196,6 +224,13 @@ public class Logger {
         } else {
             // Format the message if we have arguments
             formattedMessage = (args == null || args.length == 0) ? message : String.format(Locale.ROOT, message, args);
+        }
+
+        // Call through to listeners
+        synchronized (listeners) {
+            for (LoggerListener listener : new ArrayList<>(listeners)) {
+                listener.onLog(priority, throwable, formattedMessage);
+            }
         }
 
         // Log directly if we do not have a throwable
---
+++ urbanairship-core/src/main/java/com/urbanairship/LoggerListener.java
@@ -0,0 +1,19 @@
+/* Copyright 2018 Urban Airship and Contributors */
+
+package com.urbanairship;
+
+import android.support.annotation.Nullable;
+
+/**
+ * Listener interface for the Urban Airship logger.
+ */
+public interface LoggerListener {
+    /**
+     * Called when a message is logged.
+     *
+     * @param priority The log priority.
+     * @param throwable An optional throwable.
+     * @param message The message.
+     */
+     void onLog(int priority, @Nullable Throwable throwable, @Nullable String message);
+}
