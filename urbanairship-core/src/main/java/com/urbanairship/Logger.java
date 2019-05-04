99e92c4ddabfacb08d5b85e271ef27fa4f62b0bd
David Crow <jdavidcrow@gmail.com>
2019-05-03 17:18:57 -0700

    [MOBILE-557] Refactor logger (#549)
    * [MOBILE-557] add logging core class
    
    * [MOBILE-557] update logger to use logging core class
    
    * [MOBILE-557] comments

--- urbanairship-core/src/main/java/com/urbanairship/Logger.java
+++ urbanairship-core/src/main/java/com/urbanairship/Logger.java
@@ -9,9 +9,7 @@ import android.util.Log;
 
 import com.urbanairship.util.UAStringUtil;
 
-import java.util.List;
 import java.util.Locale;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Shared logging wrapper for all Airship log entries.
@@ -23,66 +21,7 @@ public class Logger {
     @NonNull
     static String DEFAULT_TAG = "UALib";
 
-    /**
-     * The current log tag.
-     * Defaults to "UALib".
-     */
-    private static String tag = DEFAULT_TAG;
-
-    /**
-     * The current log level, as defined by <code>android.util.Log</code>.
-     * Defaults to <code>android.util.Log.ERROR</code>.
-     */
-    private static int logLevel = Log.ERROR;
-
-    /**
-     * A list of listeners.
-     */
-    private static final List<LoggerListener> listeners = new CopyOnWriteArrayList<>();
-
-    /**
-     * Default logger.
-     */
-    private static final LoggerListener defaultLogger = new LoggerListener() {
-        @Override
-        public void onLog(int priority, @Nullable Throwable throwable, @Nullable String message) {
-            // Log directly if we do not have a throwable
-            if (throwable == null) {
-                if (priority == Log.ASSERT) {
-                    Log.wtf(tag, message);
-                } else {
-                    Log.println(priority, tag, message);
-                }
-                return;
-            }
-
-            // Log using one of the provided log methods
-            switch (priority) {
-                case Log.INFO:
-                    Log.i(tag, message, throwable);
-                    break;
-                case Log.DEBUG:
-                    Log.d(tag, message, throwable);
-                    break;
-                case Log.VERBOSE:
-                    Log.v(tag, message, throwable);
-                    break;
-                case Log.WARN:
-                    Log.w(tag, message, throwable);
-                    break;
-                case Log.ERROR:
-                    Log.e(tag, message, throwable);
-                    break;
-                case Log.ASSERT:
-                    Log.wtf(tag, message, throwable);
-                    break;
-            }
-        }
-    };
-
-    static {
-        listeners.add(defaultLogger);
-    }
+    private static LoggingCore logger = new LoggingCore(Log.ERROR, DEFAULT_TAG);
 
     /**
      * Private, unused constructor
@@ -96,7 +35,7 @@ public class Logger {
      * @param tag The tag.
      */
     public static void setTag(@NonNull String tag) {
-        Logger.tag = tag;
+        logger.setTag(tag);
     }
 
     /**
@@ -107,7 +46,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void setLogLevel(int logLevel) {
-        Logger.logLevel = logLevel;
+        logger.setLogLevel(logLevel);
     }
 
     /**
@@ -117,14 +56,14 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static int getLogLevel() {
-        return logLevel;
+        return logger.getLogLevel();
     }
 
     /**
      * Disables Airship from using the standard {@link Log} for logging.
      */
     public static void disableDefaultLogger() {
-        removeListener(defaultLogger);
+        logger.setDefaultLoggerEnabled(false);
     }
 
     /**
@@ -136,7 +75,7 @@ public class Logger {
      * @param listener The listener.
      */
     public static void addListener(@NonNull LoggerListener listener) {
-        listeners.add(listener);
+        logger.addListener(listener);
     }
 
     /**
@@ -145,7 +84,7 @@ public class Logger {
      * @param listener The listener.
      */
     public static void removeListener(@NonNull LoggerListener listener) {
-        listeners.remove(listener);
+        logger.removeListener(listener);
     }
 
     /**
@@ -157,7 +96,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void warn(@NonNull String message, @Nullable Object... args) {
-        log(Log.WARN, null, message, args);
+        logger.log(Log.WARN, null, message, args);
     }
 
     /**
@@ -170,7 +109,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void warn(@NonNull Throwable t, @NonNull String message, @Nullable Object... args) {
-        log(Log.WARN, t, message, args);
+        logger.log(Log.WARN, t, message, args);
     }
 
     /**
@@ -181,7 +120,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void warn(@NonNull Throwable t) {
-        log(Log.WARN, t, null);
+        logger.log(Log.WARN, t, null, null);
     }
 
     /**
@@ -193,7 +132,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void verbose(@NonNull String message, @Nullable Object... args) {
-        log(Log.VERBOSE, null, message, args);
+        logger.log(Log.VERBOSE, null, message, args);
     }
 
     /**
@@ -205,7 +144,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void debug(@NonNull String message, @Nullable Object... args) {
-        log(Log.DEBUG, null, message, args);
+        logger.log(Log.DEBUG, null, message, args);
     }
 
     /**
@@ -218,7 +157,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void debug(@NonNull Throwable t, @NonNull String message, @Nullable Object... args) {
-        log(Log.DEBUG, t, message, args);
+        logger.log(Log.DEBUG, t, message, args);
     }
 
     /**
@@ -230,7 +169,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void info(@NonNull String message, @NonNull Object... args) {
-        log(Log.INFO, null, message, args);
+        logger.log(Log.INFO, null, message, args);
     }
 
     /**
@@ -243,7 +182,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void info(@NonNull Throwable t, @NonNull String message, @Nullable Object... args) {
-        log(Log.INFO, t, message, args);
+        logger.log(Log.INFO, t, message, args);
     }
 
     /**
@@ -255,7 +194,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void error(@NonNull String message, @Nullable Object... args) {
-        log(Log.ERROR, null, message, args);
+        logger.log(Log.ERROR, null, message, args);
     }
 
     /**
@@ -266,7 +205,7 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void error(@NonNull Throwable t) {
-        log(Log.ERROR, t, null);
+        logger.log(Log.ERROR, t, null, null);
     }
 
     /**
@@ -279,39 +218,8 @@ public class Logger {
      */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static void error(@NonNull Throwable t, @NonNull String message, @Nullable Object... args) {
-        log(Log.ERROR, t, message, args);
-    }
+        logger.log(Log.ERROR, t, message, args);
 
-    /**
-     * Helper method that performs the logging.
-     *
-     * @param priority The log priority level.
-     * @param throwable The optional exception.
-     * @param message The optional message.
-     * @param args The optional message args.
-     */
-    private static void log(int priority, @Nullable Throwable throwable, @Nullable String message, @Nullable Object... args) {
-        if (logLevel > priority) {
-            return;
-        }
-
-        if (message == null && throwable == null) {
-            return;
-        }
-
-        String formattedMessage;
-
-        if (UAStringUtil.isEmpty(message)) {
-            // Default to empty string
-            formattedMessage = "";
-        } else {
-            // Format the message if we have arguments
-            formattedMessage = (args == null || args.length == 0) ? message : String.format(Locale.ROOT, message, args);
-        }
-
-        for (LoggerListener listener : listeners) {
-            listener.onLog(priority, throwable, formattedMessage);
-        }
     }
 
     /**
@@ -349,7 +257,7 @@ public class Logger {
                 return intValue;
             }
 
-            Logger.warn("%s is not a valid log level. Falling back to %s.", intValue, defaultValue);
+            warn("%s is not a valid log level. Falling back to %s.", intValue, defaultValue);
             return defaultValue;
         } catch (NumberFormatException nfe) {
             throw new IllegalArgumentException("Invalid log level: " + value);
---
+++ urbanairship-core/src/main/java/com/urbanairship/LoggingCore.java
@@ -0,0 +1,166 @@
+/* Copyright Airship and Contributors */
+
+package com.urbanairship;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.urbanairship.util.UAStringUtil;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Shared logging wrapper for all log entries.
+ * This class serves to consolidate the tag and log level in a
+ * single location.
+ */
+public class LoggingCore {
+
+    private String logTag;
+    private int logLevel;
+    private boolean isDefaultLoggerEnabled = true;
+
+
+    /**
+     * A list of listeners.
+     */
+    private final List<LoggerListener> listeners = new CopyOnWriteArrayList<>();
+
+    /**
+     * The logging core constructor.
+     *
+     * @param logLevel The loggin level.
+     * @param tag The log tag.
+     */
+    public LoggingCore(int logLevel, @NonNull String tag) {
+        this.logLevel = logLevel;
+        this.logTag = tag;
+    }
+
+    /**
+     * Sets the log tag.
+     *
+     * @param tag The log tag.
+     */
+    public void setTag(@NonNull String tag) {
+        this.logTag = tag;
+    }
+
+    /**
+     * Sets the enabled default logger flag.
+     *
+     * @param enabled The enable default logger flag.
+     */
+    public void setDefaultLoggerEnabled(boolean enabled) {
+        this.isDefaultLoggerEnabled = enabled;
+    }
+
+    /**
+     * Adds a listener.
+     *
+     * Listener callbacks are synchronized but will be made from the originating thread.
+     * Responsibility for any additional threading guarantees falls on the application.
+     *
+     * @param listener The listener.
+     */
+    public void addListener(@NonNull LoggerListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Removes a listener.
+     *
+     * @param listener The listener.
+     */
+    public void removeListener(@NonNull LoggerListener listener) {
+        listeners.remove(listener);
+    }
+
+    /**
+     * Helper method that performs the logging.
+     *
+     * @param priority The log priority level.
+     * @param throwable The optional exception.
+     * @param message The optional message.
+     * @param args The optional message args.
+     */
+    public void log(int priority, @Nullable Throwable throwable, @Nullable String message, @Nullable Object... args) {
+        if (logLevel > priority) {
+            return;
+        }
+
+        if (message == null && throwable == null) {
+            return;
+        }
+
+        String formattedMessage;
+
+        if (UAStringUtil.isEmpty(message)) {
+            // Default to empty string
+            formattedMessage = "";
+        } else {
+            // Format the message if we have arguments
+            formattedMessage = (args == null || args.length == 0) ? message : String.format(Locale.ROOT, message, args);
+        }
+
+        for (LoggerListener listener : listeners) {
+            listener.onLog(priority, throwable, formattedMessage);
+        }
+
+        if (isDefaultLoggerEnabled) {
+            // Log directly if we do not have a throwable
+            if (throwable == null) {
+                if (priority == Log.ASSERT) {
+                    Log.wtf(logTag, message);
+                } else {
+                    Log.println(priority, logTag, message);
+                }
+                return;
+            }
+
+            // Log using one of the provided log methods
+            switch (priority) {
+                case Log.INFO:
+                    Log.i(logTag, message, throwable);
+                    break;
+                case Log.DEBUG:
+                    Log.d(logTag, message, throwable);
+                    break;
+                case Log.VERBOSE:
+                    Log.v(logTag, message, throwable);
+                    break;
+                case Log.WARN:
+                    Log.w(logTag, message, throwable);
+                    break;
+                case Log.ERROR:
+                    Log.e(logTag, message, throwable);
+                    break;
+                case Log.ASSERT:
+                    Log.wtf(logTag, message, throwable);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Sets the log level.
+     *
+     * @param logLevel The log priority level.
+     */
+    public void setLogLevel(int logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    /**
+     * Gets the log level.
+     *
+     * @return The log priority level.
+     */
+    public int getLogLevel() {
+        return logLevel;
+    }
+
+}
