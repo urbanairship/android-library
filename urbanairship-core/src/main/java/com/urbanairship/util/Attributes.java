007a434386e3d9c0c075d257fb40dd90aaf9b077
Benjamin Benoit <>
2020-07-20 17:23:39 +0200

    Attributes file

---
+++ urbanairship-core/src/main/java/com/urbanairship/util/Attributes.java
@@ -0,0 +1,32 @@
+package com.urbanairship.util;
+/* Copyright Airship and Contributors */
+
+import java.util.Date;
+
+/**
+ * Predefined attributes.
+ *
+ * @hide
+ */
+public class Attributes {
+    private static final String UAAttributeTitle = "title";
+    private static final String UAAttributeFirstName = "first_name";
+    private static final String UAAttributeLastName = "last_name";
+    private static final String UAAttributeFullName = "full_name";
+    private static final String UAAttributeGender = "gender";
+    private static final Number UAAttributeZipCode = null;
+    private static final String UAAttributeCity = "city";
+    private static final String UAAttributeRegion = "region";
+    private static final String UAAttributeCountry = "country";
+    private static final Date UAAttributeBirthdate = null;
+    private static final Number UAAttributeAge = null;
+    private static final Number UAAttributeMobilePhone = null;
+    private static final Number UAAttributeHomePhone = null;
+    private static final Number UAAttributeWorkPhone = null;
+    private static final String UAAttributeLoyaltyTier = "loyalty_tier";
+    private static final String UAAttributeCompany = "company";
+    private static final String UAAttributeUsername = "username";
+    private static final Date UAAttributeAccountCreation = null;
+    private static final String UAAttributeEmail = "email";
+    private static final String UAAttributeAdvertisingId = "advertising_id";
+}
