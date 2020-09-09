53ee21303e529f43a20954bb8e037f6576574084
Apekka <antoine.pery-kasza@airship.com>
2020-09-09 16:30:27 +0200

    Handling failure, using Airship's Request class, and better LoadingActivity

--- urbanairship-core/src/main/java/com/urbanairship/actions/LoadingActivity.java
+++ urbanairship-core/src/main/java/com/urbanairship/actions/LoadingActivity.java
@@ -1,19 +1,25 @@
 package com.urbanairship.actions;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
 
+import com.urbanairship.AirshipExecutors;
 import com.urbanairship.Logger;
 import com.urbanairship.R;
+import com.urbanairship.http.Request;
+import com.urbanairship.http.RequestException;
+import com.urbanairship.http.Response;
+import com.urbanairship.http.ResponseParser;
+import com.urbanairship.util.UAHttpStatusUtil;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -39,23 +45,45 @@ public class LoadingActivity extends AppCompatActivity {
             finish();
         }
 
-        try {
-            // Get the real Google Pay URL and then navigate to it
-            HttpURLConnection httpURLConnection;
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setInstanceFollowRedirects(false);
-            String redirectURL = httpURLConnection.getHeaderField("Location");
+        AirshipExecutors.THREAD_POOL_EXECUTOR.execute(new Runnable() {
 
-            if (redirectURL != null) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(redirectURL));
-                startActivity(browserIntent);
-            } else {
-                Logger.warn("No redirection found for the Wallet URL. Finishing action.");
+            private int maxRetries = 5;
+
+            @Override
+            public void run() {
+                try {
+                    Response<String> response = new Request()
+                            .setOperation("GET", url)
+                            .setInstanceFollowRedirects(false)
+                            .execute(new ResponseParser<String>() {
+                                @Override
+                                public String parseResponse(int status, @Nullable Map<String, List<String>> headers, @Nullable String responseBody) {
+                                    if (UAHttpStatusUtil.inRedirectionRange(status)) {
+                                        if (headers != null && headers.get("Location") != null) {
+                                            return headers.get("Location").get(0);
+                                        }
+                                    }
+                                    return null;
+                                }
+                            });
+                    if (response.getResult() != null) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(response.getResponseHeader("Location")));
+                        startActivity(browserIntent);
+                    } else {
+                        Logger.warn("No redirection found for the Wallet URL. Finishing action.");
+                        finish();
+                    }
+                } catch (RequestException e) {
+                    maxRetries--;
+                    if (maxRetries > 0) {
+                        Logger.warn("Wallet action request error, trying again, tries left : " + maxRetries);
+                        AirshipExecutors.THREAD_POOL_EXECUTOR.submit(this);
+                    } else {
+                        AirshipExecutors.THREAD_POOL_EXECUTOR.shutdown();
+                        finish();
+                    }
+                }
             }
-
-            finish();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
     }
 }
