cbbf28cb69601312b863d2fb09754810750672b6
Apekka <antoine.pery-kasza@airship.com>
2021-03-11 14:34:54 +0100

    fix Unit Test

--- urbanairship-automation/src/test/java/com/urbanairship/automation/NetworkMonitorTest.java
+++ urbanairship-automation/src/test/java/com/urbanairship/automation/NetworkMonitorTest.java
@@ -2,21 +2,21 @@ package com.urbanairship.automation;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
 
 import com.urbanairship.util.Network;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.shadows.ShadowNetworkCapabilities;
-import org.robolectric.shadows.ShadowNetworkInfo;
+import org.robolectric.shadows.ShadowConnectivityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.robolectric.Shadows.shadowOf;
@@ -24,34 +24,29 @@ import static org.robolectric.Shadows.shadowOf;
 @RunWith(AndroidJUnit4.class)
 public class NetworkMonitorTest {
 
-    private Context context;
-
     @Test
     public void testNetworkResumed() {
+        android.net.Network mockNetwork = mock(android.net.Network.class);
+        Network.ConnectionListener mockConnectionListener = mock(Network.ConnectionListener.class);
 
-        context = ApplicationProvider.getApplicationContext();
+        Context context = ApplicationProvider.getApplicationContext();
 
-        Network.ConnectionListener connectionListener = mock(Network.ConnectionListener.class);
+        ShadowConnectivityManager shadowConnectivityManager =
+                shadowOf((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+
         NetworkMonitor networkMonitor = new NetworkMonitor();
+        networkMonitor.setConnectionListener(mockConnectionListener);
 
-        networkMonitor.setConnectionListener(connectionListener);
+        Set<ConnectivityManager.NetworkCallback> networkCallbacks = shadowConnectivityManager.getNetworkCallbacks();
+        assertEquals(1, networkCallbacks.size());
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        List<ConnectivityManager.NetworkCallback> callbacks = new ArrayList<>(networkCallbacks);
+        ConnectivityManager.NetworkCallback callback = callbacks.get(0);
 
-        NetworkCapabilities networkCapabilities = ShadowNetworkCapabilities.newInstance();
-        shadowOf(networkCapabilities).removeTransportType(NetworkCapabilities.TRANSPORT_WIFI);
-        NetworkInfo networkInfo =  ShadowNetworkInfo.newInstance(NetworkInfo.DetailedState.DISCONNECTED, ConnectivityManager.TYPE_WIFI, 0, false, false);
-        shadowOf(connectivityManager).setActiveNetworkInfo(networkInfo);
-        //shadowOf(connectivityManager).setNetworkCapabilities(connectivityManager.getActiveNetwork(), networkCapabilities);
+        callback.onAvailable(mockNetwork);
+        verify(mockConnectionListener).onConnectionChanged(true);
 
-        assertFalse(Network.isConnected());
-
-        NetworkInfo connectedNetworkInfo =  ShadowNetworkInfo.newInstance(NetworkInfo.DetailedState.CONNECTED, ConnectivityManager.TYPE_WIFI, 0, true, true);
-        shadowOf(connectivityManager).setActiveNetworkInfo(connectedNetworkInfo);
-
-        assertTrue(Network.isConnected());
-        verify(connectionListener).onConnectionChanged(false);
-
+        callback.onLost(mockNetwork);
+        verify(mockConnectionListener).onConnectionChanged(false);
     }
-
 }
