927d5933e118ee9a1fa8472aaf32592a08946079
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2021-07-19 11:04:03 -0700

    Add try/catch around network callback register and unregister in NetworkMonitor (#979)

--- urbanairship-automation/src/main/java/com/urbanairship/automation/NetworkMonitor.java
+++ urbanairship-automation/src/main/java/com/urbanairship/automation/NetworkMonitor.java
@@ -7,9 +7,11 @@ import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
 import android.os.Build;
 
+import com.urbanairship.Logger;
 import com.urbanairship.UAirship;
 import com.urbanairship.util.Network.ConnectionListener;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -17,15 +19,15 @@ public class NetworkMonitor {
 
     private ConnectionListener connectionListener;
 
-    private ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+    private final ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
         @Override
-        public void onAvailable(Network network) {
+        public void onAvailable(@NonNull Network network) {
             if (connectionListener != null) {
                 connectionListener.onConnectionChanged(true);
             }
         }
 
-        public void onLost(Network network) {
+        public void onLost(@NonNull Network network) {
             if (connectionListener != null) {
                 connectionListener.onConnectionChanged(false);
             }
@@ -36,12 +38,18 @@ public class NetworkMonitor {
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) UAirship.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            connectivityManager.registerDefaultNetworkCallback(networkCallback);
-        } else {
-            NetworkRequest request = new NetworkRequest.Builder()
-                    .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build();
-            connectivityManager.registerNetworkCallback(request, networkCallback);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                connectivityManager.registerDefaultNetworkCallback(networkCallback);
+            } else {
+                NetworkRequest request = new NetworkRequest.Builder()
+                        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build();
+                connectivityManager.registerNetworkCallback(request, networkCallback);
+            }
+        } catch (SecurityException e) {
+            // Workaround "Package android does not belong to 10246" exceptions that occur on
+            // a small set of devices.
+            Logger.warn(e, "NetworkMonitor failed to register network callback!");
         }
     }
 
@@ -49,7 +57,12 @@ public class NetworkMonitor {
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) UAirship.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        connectivityManager.unregisterNetworkCallback(networkCallback);
+        try {
+            connectivityManager.unregisterNetworkCallback(networkCallback);
+        } catch (Exception e) {
+            // Ignore unregister errors in case the callback wasn't previously registered.
+            Logger.warn(e, "NetworkMonitor failed to unregister network callback!");
+        }
     }
 
     public void setConnectionListener(ConnectionListener listener) {
