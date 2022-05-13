365f7b4d35d5a167c7d99ecb2586bc38c717aaac
Antoine PERY-KASZA <antoine.pery-kasza@airship.com>
2022-05-14 00:49:06 +0200

    Update ADM to 1.1.0 (#1129)

---
+++ urbanairship-adm/src/main/java/com/urbanairship/push/adm/AdmHandlerBase.java
@@ -0,0 +1,42 @@
+/* Copyright Airship and Contributors */
+
+package com.urbanairship.push.adm;
+
+import android.content.Intent;
+
+import com.amazon.device.messaging.ADMMessageHandlerBase;
+import com.urbanairship.Autopilot;
+import com.urbanairship.Logger;
+import com.urbanairship.push.PushMessage;
+import com.urbanairship.push.PushProviderBridge;
+
+public class AdmHandlerBase extends ADMMessageHandlerBase {
+
+    //Class that is used for legacy ADM devices
+    protected AdmHandlerBase(String className) {
+        super(className);
+    }
+
+    @Override
+    protected void onMessage(Intent intent) {
+        Autopilot.automaticTakeOff(getApplicationContext());
+        final PushMessage message = new PushMessage(intent.getExtras());
+        PushProviderBridge.processPush(AdmPushProvider.class, message).executeSync(getApplicationContext());
+    }
+
+    @Override
+    protected void onRegistrationError(String errorId) {
+        Logger.error("An error occured during ADM Registration : " + errorId);
+    }
+
+    @Override
+    protected void onRegistered(String newRegistrationId) {
+        PushProviderBridge.requestRegistrationUpdate(getApplicationContext(), AdmPushProvider.class, newRegistrationId);
+    }
+
+    @Override
+    protected void onUnregistered(String newRegistrationId) {
+        PushProviderBridge.requestRegistrationUpdate(getApplicationContext(), AdmPushProvider.class, newRegistrationId);
+    }
+
+}
--- urbanairship-adm/src/main/java/com/urbanairship/push/adm/AdmPushReceiver.java
+++ urbanairship-adm/src/main/java/com/urbanairship/push/adm/AdmPushReceiver.java
@@ -2,19 +2,8 @@
 
 package com.urbanairship.push.adm;
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-import com.amazon.device.messaging.ADMConstants;
-import com.urbanairship.Autopilot;
+import com.amazon.device.messaging.ADMMessageReceiver;
 import com.urbanairship.Logger;
-import com.urbanairship.push.PushMessage;
-import com.urbanairship.push.PushProviderBridge;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
 
 /**
@@ -22,45 +11,17 @@ import androidx.annotation.RestrictTo;
  * @hide
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-public class AdmPushReceiver extends BroadcastReceiver {
+public class AdmPushReceiver extends ADMMessageReceiver {
 
-    /**
-     * Amount of time in milliseconds a broadcast receiver has to process an intent.
-     */
-    private static final long BROADCAST_INTENT_TIME_MS = 10000;
+    public AdmPushReceiver() {
+        super(AdmHandlerBase.class);
 
-    @Override
-    public void onReceive(@NonNull final Context context, @Nullable final Intent intent) {
-        Autopilot.automaticTakeOff(context);
-
-        if (intent == null || intent.getExtras() == null || !ADMConstants.LowLevel.ACTION_RECEIVE_ADM_MESSAGE.equals(intent.getAction())) {
-            if (isOrderedBroadcast()) {
-                setResultCode(Activity.RESULT_OK);
-            }
-            return;
+        //Check if the latest ADM version is available on the device
+        try {
+            Class.forName("com.amazon.device.messaging.ADMMessageHandlerJobBase");
+            registerJobServiceClass(AdmHandlerJobBase.class, 3004);
+        } catch (ClassNotFoundException e) {
+            Logger.warn("Using legacy ADM class : " + e.getMessage());
         }
-
-        final boolean isOrderedBroadcast = isOrderedBroadcast();
-        final PendingResult result = goAsync();
-        final PushMessage message = new PushMessage(intent.getExtras());
-
-        Logger.verbose("Received push.");
-
-        PushProviderBridge.processPush(AdmPushProvider.class, message)
-                          .setMaxCallbackWaitTime(BROADCAST_INTENT_TIME_MS)
-                          .execute(context, new Runnable() {
-                              @Override
-                              public void run() {
-                                  if (result == null) {
-                                      return;
-                                  }
-
-                                  if (isOrderedBroadcast) {
-                                      result.setResultCode(Activity.RESULT_OK);
-                                  }
-                                  result.finish();
-                              }
-                          });
     }
-
 }
