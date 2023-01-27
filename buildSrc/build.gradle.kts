99c5a355d0743b96f9628c9459ab769db445a841
umair <umair@freetrade.io>
2023-01-27 23:01:56 +0000

    update java

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -40,8 +40,8 @@ gradlePlugin {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
