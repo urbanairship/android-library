7688c0e2eaf0cf13247162076217517dfdf8644e
umair <umair@freetrade.io>
2023-01-28 00:36:31 +0000

    revert java version

--- buildSrc/src/main/kotlin/AirshipModulePlugin.kt
+++ buildSrc/src/main/kotlin/AirshipModulePlugin.kt
@@ -78,8 +78,8 @@ class AirshipModulePlugin : Plugin<Project> {
             }
 
             compileOptions.apply {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
+                sourceCompatibility = JavaVersion.VERSION_1_8
+                targetCompatibility = JavaVersion.VERSION_1_8
             }
 
             lint {
