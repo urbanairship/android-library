0baad9491b5302f03afd33e909fa6c225d265fe3
Ryan Lepinski <ryan.lepinski@airship.com>
2023-02-13 11:17:36 -0800

    Bump versions (#1185)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -34,12 +34,12 @@ gradlePlugin {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
