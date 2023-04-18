afddf1f3586b480072dc2b08cfcd546adf3a733b
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2023-04-18 15:34:30 -0700

    [MOBILE-3601] Fix pager page actions (#1226)
    * Fix actions for initial pager page displays
    
    * Update PagerModelTest
    
    * spots

--- urbanairship-layout/src/main/java/com/urbanairship/android/layout/model/PagerModel.kt
+++ urbanairship-layout/src/main/java/com/urbanairship/android/layout/model/PagerModel.kt
@@ -18,6 +18,8 @@ import com.urbanairship.android.layout.property.ViewType
 import com.urbanairship.android.layout.util.pagerScrolls
 import com.urbanairship.android.layout.view.PagerView
 import com.urbanairship.json.JsonValue
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 internal class PagerModel(
@@ -82,6 +84,25 @@ internal class PagerModel(
         pagerState.update { state ->
             state.copyWithPageIds(pageIds = items.map { it.identifier })
         }
+
+        // Listen for page changes (or the initial page display)
+        // and run any actions for the current page.
+        modelScope.launch {
+
+            pagerState.changes
+                .map { it.pageIndex to it.lastPageIndex }
+                .filter { (pageIndex, lastPageIndex) ->
+                    // If current and last are both 0, we're initializing the pager.
+                    // Otherwise, we only want to act on changes to the pageIndex.
+                    pageIndex == 0 && lastPageIndex == 0 || pageIndex != lastPageIndex
+                }
+                .collect { (pageIndex, _) ->
+                    // Run any actions for the current page.
+                    items[pageIndex].actions?.let { actions ->
+                        runActions(actions)
+                    }
+                }
+        }
     }
 
     override fun onCreateView(context: Context, viewEnvironment: ViewEnvironment) =
@@ -90,12 +111,15 @@ internal class PagerModel(
         }
 
     override fun onViewAttached(view: PagerView) {
+        // Collect page index changes from state and tell the view to scroll to the current page.
         viewScope.launch {
             pagerState.changes.collect {
                 listener?.scrollTo(it.pageIndex)
             }
         }
 
+        // Collect pager scrolls, update pager state, and report
+        // the page swipe if it was triggered by the user.
         viewScope.launch {
             view.pagerScrolls().collect { (position, isInternalScroll) ->
                 pagerState.update { state ->
@@ -104,11 +128,6 @@ internal class PagerModel(
 
                 if (!isInternalScroll) {
                     reportPageSwipe(pagerState.changes.value)
-                }
-
-                // Run any actions for the current page.
-                items[position].actions?.let { actions ->
-                    runActions(actions)
                 }
             }
         }
