308ace2cba16c86da743115f6aaf51b80e603213
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2023-05-24 14:11:48 -0700

    Fix sms, tel, and mailto urls in webviews (#1255)

--- urbanairship-core/src/main/java/com/urbanairship/webkit/AirshipWebChromeClient.java
+++ urbanairship-core/src/main/java/com/urbanairship/webkit/AirshipWebChromeClient.java
@@ -26,11 +26,16 @@ import com.urbanairship.javascript.NativeBridge;
 import com.urbanairship.util.UriUtils;
 
 import java.lang.ref.WeakReference;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Web Chrome Client that enables full screen video.
  */
 public class AirshipWebChromeClient extends WebChromeClient {
+
+    /** Special schemes for URLs that don't have a host. */
+    private final static List<String> SPECIAL_SCHEMES = Arrays.asList("tel", "sms", "mailto");
 
     private final WeakReference<Activity> weakActivity;
     private View customView;
@@ -101,7 +106,9 @@ public class AirshipWebChromeClient extends WebChromeClient {
                     if (url != null) {
                         Uri uri = Uri.parse(url);
 
-                        if (uri.getHost() == null || NativeBridge.UA_ACTION_SCHEME.equals(uri.getScheme())) {
+                        boolean isActionUrl = NativeBridge.UA_ACTION_SCHEME.equals(uri.getScheme());
+                        boolean hasSpecialScheme = SPECIAL_SCHEMES.contains(uri.getScheme());
+                        if ((!hasSpecialScheme && uri.getHost() == null) || isActionUrl) {
                             return false;
                         }
 
