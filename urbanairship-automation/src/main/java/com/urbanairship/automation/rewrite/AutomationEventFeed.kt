e248a27c490a0024e67fea247f6c0d70b4663200
ilitvinenko <mobexire6@gmail.com>
2024-03-11 12:55:35 +0200

    MOBILE-4183: Event feed for Kotlin rewrite of AutomationEngine (#1378)
    * added automation event feed

---
+++ urbanairship-automation/src/main/java/com/urbanairship/automation/rewrite/AutomationEventFeed.kt
@@ -0,0 +1,140 @@
+package com.urbanairship.automation.rewrite
+
+import com.urbanairship.AirshipDispatchers
+import com.urbanairship.ApplicationMetrics
+import com.urbanairship.analytics.Analytics
+import com.urbanairship.analytics.AnalyticsListener
+import com.urbanairship.analytics.CustomEvent
+import com.urbanairship.analytics.Event
+import com.urbanairship.analytics.location.RegionEvent
+import com.urbanairship.app.ActivityMonitor
+import com.urbanairship.app.ApplicationListener
+import com.urbanairship.json.JsonValue
+import java.util.UUID
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+internal data class TriggerableState(
+    var appSessionID: String? = null,
+    var versionUpdated: String? = null
+)
+
+internal sealed class AutomationEvent {
+    data object Foreground : AutomationEvent()
+    data object Background : AutomationEvent()
+    data object AppInit : AutomationEvent()
+    data class ScreenView(val name: String?): AutomationEvent()
+    data class StateChanged(val state: TriggerableState) : AutomationEvent()
+    data class RegionEnter(val data: JsonValue) : AutomationEvent()
+    data class RegionExit(val data: JsonValue) : AutomationEvent()
+    data class CustomEvent(val data: JsonValue, val count: Double?) : AutomationEvent()
+    data class FeatureFlagInteracted(val data: JsonValue) : AutomationEvent()
+}
+
+private interface Cancellable {
+    fun cancel()
+}
+
+internal class AutomationEventFeed(
+    private val applicationMetrics: ApplicationMetrics,
+    private val activityMonitor: ActivityMonitor,
+    private val analytics: Analytics
+) {
+    private val stream = MutableSharedFlow<AutomationEvent>()
+    private val appSessionState = TriggerableState()
+    private var subscription: Cancellable? = null
+    private val scope = CoroutineScope(AirshipDispatchers.IO + SupervisorJob())
+    private var isFirstAttach = false
+
+    val feed: Flow<AutomationEvent> = stream
+
+    fun attach() {
+        if (subscription != null) { return }
+
+        if (!isFirstAttach) {
+            isFirstAttach = true
+            emit(AutomationEvent.AppInit)
+
+            if (applicationMetrics.appVersionUpdated) {
+                appSessionState.versionUpdated = applicationMetrics.currentAppVersion.toString()
+                emit(AutomationEvent.StateChanged(appSessionState))
+            }
+        }
+
+        subscription = startListeningForEvents()
+    }
+
+    fun detach() {
+        subscription?.cancel()
+    }
+
+    private fun emit(event: AutomationEvent) {
+        runBlocking { stream.emit(event) }
+
+        when (event) {
+            AutomationEvent.Foreground -> setAppSessionId(UUID.randomUUID().toString())
+            AutomationEvent.Background -> setAppSessionId(null)
+            else -> {}
+        }
+    }
+
+    private fun setAppSessionId(id: String?) {
+        if (appSessionState.appSessionID == id) { return }
+
+        appSessionState.appSessionID = id
+        emit(AutomationEvent.StateChanged(appSessionState))
+    }
+
+    private fun startListeningForEvents(): Cancellable {
+        val appStateListener = object : ApplicationListener {
+            override fun onForeground(milliseconds: Long) {
+                emit(AutomationEvent.Foreground)
+            }
+
+            override fun onBackground(milliseconds: Long) {
+                emit(AutomationEvent.Background)
+            }
+        }
+        activityMonitor.addApplicationListener(appStateListener)
+
+        val analyticsListener = object : AnalyticsListener {
+            override fun onScreenTracked(screenName: String) {
+                emit(AutomationEvent.ScreenView(screenName))
+            }
+
+            override fun onCustomEventAdded(event: CustomEvent) {
+                emit(AutomationEvent.CustomEvent(
+                    data = event.properties.toJsonValue(),
+                    count = event.eventValue?.toDouble()
+                ))
+            }
+
+            override fun onRegionEventAdded(event: RegionEvent) {
+                val trackEvent = when (event.boundaryEvent) {
+                    RegionEvent.BOUNDARY_EVENT_ENTER -> AutomationEvent.RegionEnter(event.eventData.toJsonValue())
+                    RegionEvent.BOUNDARY_EVENT_EXIT -> AutomationEvent.RegionExit(event.eventData.toJsonValue())
+                    else -> null
+                }
+
+                trackEvent?.let { emit(it) }
+            }
+
+            override fun onFeatureFlagInteractedEventAdded(event: Event) {
+                emit(AutomationEvent.FeatureFlagInteracted(event.eventData.toJsonValue()))
+            }
+        }
+        analytics.addAnalyticsListener(analyticsListener)
+
+        return object : Cancellable {
+            override fun cancel() {
+                activityMonitor.removeApplicationListener(appStateListener)
+                analytics.removeAnalyticsListener(analyticsListener)
+            }
+        }
+    }
+}
---
+++ urbanairship-automation/src/test/java/com/urbanairship/automation/AutomationEventFeedTest.kt
@@ -0,0 +1,190 @@
+package com.urbanairship.automation
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.urbanairship.ApplicationMetrics
+import com.urbanairship.PreferenceDataStore
+import com.urbanairship.PrivacyManager
+import com.urbanairship.TestActivityMonitor
+import com.urbanairship.TestAirshipRuntimeConfig
+import com.urbanairship.analytics.Analytics
+import com.urbanairship.analytics.CustomEvent
+import com.urbanairship.analytics.Event
+import com.urbanairship.analytics.location.RegionEvent
+import com.urbanairship.automation.rewrite.AutomationEvent
+import com.urbanairship.automation.rewrite.AutomationEventFeed
+import com.urbanairship.automation.rewrite.TriggerableState
+import com.urbanairship.json.JsonMap
+import com.urbanairship.locale.LocaleManager
+import app.cash.turbine.test
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestResult
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class AutomationEventFeedTest {
+    private val metrics: ApplicationMetrics = mockk()
+    private val activityMonitor = TestActivityMonitor()
+    private lateinit var analytics: Analytics
+    private lateinit var subject: AutomationEventFeed
+
+    @Before
+    public fun setup() {
+        every { metrics.appVersionUpdated } returns true
+        every { metrics.currentAppVersion } returns 123L
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val dataStore = PreferenceDataStore.inMemoryStore(context)
+
+        analytics = Analytics(
+            context,
+            dataStore,
+            TestAirshipRuntimeConfig(),
+            PrivacyManager(dataStore, PrivacyManager.FEATURE_ALL),
+            mockk(),
+            LocaleManager(context, dataStore),
+            mockk()
+        )
+
+        subject = AutomationEventFeed(metrics, activityMonitor, analytics)
+    }
+
+    @Test
+    public fun firstAttachProducesInitAndVersionUpdated(): TestResult = runTest {
+        subject.feed.test {
+            subject.attach()
+
+            assertEquals(AutomationEvent.AppInit, awaitItem())
+
+            val expectedState = TriggerableState(versionUpdated = "123")
+            val state = (awaitItem() as? AutomationEvent.StateChanged)?.state
+            assertEquals(expectedState, state)
+        }
+    }
+
+    @Test
+    public fun subsequentAttachEmitsNoEvents(): TestResult = runTest {
+        subject.feed.test {
+            subject.attach()
+            skipItems(2)
+
+            subject.attach()
+            expectNoEvents()
+
+            subject.detach()
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    public fun testSupportedEvents(): TestResult = runTest {
+        subject.feed.test {
+            subject.attach()
+            skipItems(2)
+
+            activityMonitor.foreground()
+            assertEquals(AutomationEvent.Foreground, awaitItem())
+
+            var state = (awaitItem() as? AutomationEvent.StateChanged)?.state
+            assertNotNull(state?.appSessionID)
+
+            activityMonitor.background()
+            assertEquals(AutomationEvent.Background, awaitItem())
+
+            state = (awaitItem() as? AutomationEvent.StateChanged)?.state
+            assertNotNull(state)
+            assertNull(state?.appSessionID)
+
+            analytics.trackScreen("test-screen")
+            val screenView = (awaitItem() as? AutomationEvent.ScreenView)
+            assertEquals("test-screen", screenView?.name)
+
+            val regionEnter = RegionEvent
+                .newBuilder()
+                .setRegionId("enter-reg-id")
+                .setBoundaryEvent(RegionEvent.BOUNDARY_EVENT_ENTER)
+                .setSource("test")
+                .build()
+
+            analytics.addEvent(regionEnter)
+
+            val regEnter = (awaitItem() as? AutomationEvent.RegionEnter)
+            assertEquals(JsonMap.newBuilder()
+                .put("region_id", "enter-reg-id")
+                .put("action", "enter")
+                .put("source", "test")
+                .build().toJsonValue() , regEnter?.data)
+
+            val regionExit = RegionEvent
+                .newBuilder()
+                .setRegionId("exit-reg-id")
+                .setBoundaryEvent(RegionEvent.BOUNDARY_EVENT_EXIT)
+                .setSource("test2")
+                .build()
+
+            analytics.addEvent(regionExit)
+
+            val regExit = (awaitItem() as? AutomationEvent.RegionExit)
+            assertEquals(JsonMap.newBuilder()
+                .put("region_id", "exit-reg-id")
+                .put("action", "exit")
+                .put("source", "test2")
+                .build().toJsonValue() , regExit?.data)
+
+            val customEventAnalytic = CustomEvent
+                .newBuilder("test-custom")
+                .setEventValue(223)
+                .setProperties(JsonMap.newBuilder().put("event", "property").build())
+                .build()
+            analytics.addEvent(customEventAnalytic)
+
+            val customEvent = awaitItem() as? AutomationEvent.CustomEvent
+            assertEquals(223.0, customEvent?.count)
+            assertEquals(JsonMap.newBuilder().put("event", "property").build().toJsonValue(), customEvent?.data)
+
+            analytics.addEvent(object : Event() {
+                override fun getType(): String = "feature_flag_interaction"
+
+                override fun getEventData(): JsonMap {
+                    return JsonMap.newBuilder().put("ff", "property").build()
+                }
+            })
+
+            val ffEvent = awaitItem() as? AutomationEvent.FeatureFlagInteracted
+            assertEquals(JsonMap.newBuilder().put("ff", "property").build().toJsonValue(), ffEvent?.data)
+        }
+    }
+
+    @Test
+    public fun testNoEventsIfNotAttached(): TestResult = runTest {
+        subject.feed.test {
+            expectNoEvents()
+            analytics.trackScreen("test")
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    public fun testNoEventsAfterDetach(): TestResult = runTest {
+        subject.feed.test {
+            subject.attach()
+            skipItems(2)
+
+            analytics.trackScreen("test")
+            awaitItem()
+
+            subject.detach()
+            analytics.trackScreen("test")
+            expectNoEvents()
+        }
+    }
+}
