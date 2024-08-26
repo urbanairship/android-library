d189b109938aaab59d6c423a09d7df4f89a9abe5
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-08-26 15:24:11 -0700

    Convert MessageCenterWebViewClient to Kotlin

--- urbanairship-message-center/src/main/java/com/urbanairship/messagecenter/webkit/MessageWebViewClient.kt
+++ urbanairship-message-center/src/main/java/com/urbanairship/messagecenter/webkit/MessageWebViewClient.kt
@@ -1,88 +1,65 @@
 /* Copyright Airship and Contributors */
+package com.urbanairship.messagecenter.webkit
 
-package com.urbanairship.messagecenter.webkit;
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.annotation.CallSuper
+import androidx.annotation.MainThread
+import androidx.annotation.RestrictTo
+import com.urbanairship.actions.ActionArguments
+import com.urbanairship.actions.ActionRunRequest
+import com.urbanairship.javascript.JavaScriptEnvironment
+import com.urbanairship.json.JsonMap
+import com.urbanairship.json.JsonValue
+import com.urbanairship.messagecenter.Message
+import com.urbanairship.messagecenter.MessageCenter
+import com.urbanairship.webkit.AirshipWebViewClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
 
-import android.os.Bundle;
-import android.webkit.WebView;
-
-import com.urbanairship.actions.ActionArguments;
-import com.urbanairship.actions.ActionRunRequest;
-import com.urbanairship.javascript.JavaScriptEnvironment;
-import com.urbanairship.json.JsonMap;
-import com.urbanairship.json.JsonValue;
-import com.urbanairship.messagecenter.Message;
-import com.urbanairship.messagecenter.MessageCenter;
-import com.urbanairship.webkit.AirshipWebViewClient;
-
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-import java.util.TimeZone;
-
-import androidx.annotation.CallSuper;
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RestrictTo;
-
-/**
- * A web view client that enables the Airship Native Bridge for Message Center.
- */
-public class MessageWebViewClient extends AirshipWebViewClient {
-
-    @NonNull
-    private static SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ", Locale.US);
-
-    static {
-        DATE_FORMATTER.setTimeZone(TimeZone.getTimeZone("UTC"));
-    }
-
-    /**
-     * Default constructor.
-     */
-    public MessageWebViewClient() {
-    }
+/** A web view client that enables the Airship Native Bridge for Message Center. */
+public open class MessageWebViewClient : AirshipWebViewClient() {
 
     /**
      * @hide
      */
-    @NonNull
-    @Override
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    protected ActionRunRequest extendActionRequest(@NonNull ActionRunRequest request, @NonNull WebView webView) {
-        Bundle metadata = new Bundle();
-        Message message = getMessage(webView);
+    override fun extendActionRequest(
+        request: ActionRunRequest,
+        webView: WebView
+    ): ActionRunRequest {
+        val metadata = Bundle()
+        val message = getMessage(webView)
         if (message != null) {
-            metadata.putString(ActionArguments.RICH_PUSH_ID_METADATA, message.getMessageId());
+            metadata.putString(ActionArguments.RICH_PUSH_ID_METADATA, message.messageId)
         }
-
-        request.setMetadata(metadata);
-        return request;
+        request.setMetadata(metadata)
+        return request
     }
 
     /**
      * @hide
      */
     @CallSuper
-    @NonNull
-    @Override
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    protected JavaScriptEnvironment.Builder extendJavascriptEnvironment(@NonNull JavaScriptEnvironment.Builder builder, @NonNull WebView webView) {
-        final Message message = getMessage(webView);
-
-        JsonMap extras = JsonMap.EMPTY_MAP;
+    override fun extendJavascriptEnvironment(
+        builder: JavaScriptEnvironment.Builder,
+        webView: WebView
+    ): JavaScriptEnvironment.Builder {
+        val message = getMessage(webView)
+        var extras = JsonMap.EMPTY_MAP
         if (message != null) {
-            extras = JsonValue.wrapOpt(message.getExtrasMap()).optMap();
+            extras = JsonValue.wrapOpt(message.extrasMap).optMap()
         }
-
-        return super.extendJavascriptEnvironment(builder,webView)
-                    .addGetter("getMessageSentDateMS", (message != null) ? message.getSentDateMS() : -1)
-                    .addGetter("getMessageId", (message != null) ? message.getMessageId() : null)
-                    .addGetter("getMessageTitle", (message != null) ? message.getTitle() : null)
-                    .addGetter("getMessageSentDate", (message != null) ? DATE_FORMATTER.format(message.getSentDate()) : null)
-                    .addGetter("getUserId", MessageCenter.shared().getUser().getId())
-                    .addGetter("getMessageExtras", extras);
-
-
+        return super.extendJavascriptEnvironment(builder, webView)
+            .addGetter("getMessageSentDateMS", message?.sentDateMS ?: -1)
+            .addGetter("getMessageId", message?.messageId)
+            .addGetter("getMessageTitle", message?.title).addGetter(
+                "getMessageSentDate",
+                if (message != null) DATE_FORMATTER.format(message.sentDate) else null
+            ).addGetter("getUserId", MessageCenter.shared().user.id)
+            .addGetter("getMessageExtras", extras)
     }
 
     /**
@@ -93,9 +70,16 @@ public class MessageWebViewClient extends AirshipWebViewClient {
      * @note This method should only be called from the main thread.
      */
     @MainThread
-    @Nullable
-    private Message getMessage(@NonNull WebView webView) {
-        return MessageCenter.shared().getInbox().getMessageByUrl(webView.getUrl());
+    private fun getMessage(webView: WebView): Message? {
+        return MessageCenter.shared().inbox.getMessageByUrl(webView.url)
     }
 
+    private companion object {
+
+        private val DATE_FORMATTER = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ", Locale.US)
+
+        init {
+            DATE_FORMATTER.timeZone = TimeZone.getTimeZone("UTC")
+        }
+    }
 }
