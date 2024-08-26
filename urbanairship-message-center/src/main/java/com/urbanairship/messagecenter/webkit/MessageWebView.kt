eb4185dd43afd3333be3410feab1d88b24168eda
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-08-26 15:24:03 -0700

    Convert MessageCenterWebView to Kotlin

--- urbanairship-message-center/src/main/java/com/urbanairship/messagecenter/webkit/MessageWebView.kt
+++ urbanairship-message-center/src/main/java/com/urbanairship/messagecenter/webkit/MessageWebView.kt
@@ -1,84 +1,36 @@
-package com.urbanairship.messagecenter.webkit;
+package com.urbanairship.messagecenter.webkit
 
-import android.content.Context;
-import android.util.AttributeSet;
+import android.content.Context
+import android.util.AttributeSet
+import com.urbanairship.messagecenter.Message
+import com.urbanairship.messagecenter.MessageCenter
+import com.urbanairship.webkit.AirshipWebView
 
-import com.urbanairship.messagecenter.Message;
-import com.urbanairship.messagecenter.MessageCenter;
-import com.urbanairship.messagecenter.User;
-import com.urbanairship.webkit.AirshipWebView;
-
-import java.util.HashMap;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-/**
- * A web view that sets settings appropriate for Airship message center content.
- */
-public class MessageWebView extends AirshipWebView {
+/** A web view that sets settings appropriate for Airship message center content. */
+public open class MessageWebView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0,
+    defResStyle: Int = 0
+): AirshipWebView(context, attrs, defStyle, defResStyle) {
 
     /**
-     * MessageWebView Constructor
-     *
-     * @param context A Context object used to access application assets.
-     */
-    public MessageWebView(@NonNull Context context) {
-        super(context);
-    }
-
-    /**
-     * MessageWebView Constructor
-     *
-     * @param context A Context object used to access application assets.
-     * @param attrs An AttributeSet passed to our parent.
-     */
-    public MessageWebView(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    /**
-     * MessageWebView Constructor
-     *
-     * @param context A Context object used to access application assets.
-     * @param attrs An AttributeSet passed to our parent.
-     * @param defStyle The default style resource ID.
-     */
-    public MessageWebView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    /**
-     * MessageWebView Constructor
-     *
-     * @param context A Context object used to access application assets.
-     * @param attrs An AttributeSet passed to our parent.
-     * @param defStyle The default style resource ID.
-     * @param defResStyle A resource identifier of a style resource that supplies default values for
-     * the view, used only if defStyle is 0 or cannot be found in the theme. Can be 0 to not
-     * look for defaults.
-     */
-    public MessageWebView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle, int defResStyle) {
-        super(context, attrs, defStyle, defResStyle);
-    }
-
-    /**
-     * Loads the web view with the {@link Message}.
+     * Loads the web view with the [Message].
      *
      * @param message The message that will be displayed.
      */
-    public void loadMessage(@NonNull Message message) {
-        User user = MessageCenter.shared().getUser();
+    public open fun loadMessage(message: Message) {
+        val user = MessageCenter.shared().user
 
         // Send authorization in the headers if the web view supports it
-        HashMap<String, String> headers = new HashMap<>();
+        val headers = HashMap<String, String>()
 
         // Set the auth
-        if (user.getId() != null && user.getPassword() != null) {
-            setClientAuthRequest(message.getMessageBodyUrl(), user.getId(), user.getPassword());
-            headers.put("Authorization", createBasicAuth(user.getId(), user.getPassword()));
+        val (userId, password) = user.id to user.password
+        if (userId != null && password != null) {
+            setClientAuthRequest(message.messageBodyUrl, userId, password)
+            headers["Authorization"] = createBasicAuth(userId, password)
         }
-
-        loadUrl(message.getMessageBodyUrl(), headers);
+        loadUrl(message.messageBodyUrl, headers)
     }
 }
