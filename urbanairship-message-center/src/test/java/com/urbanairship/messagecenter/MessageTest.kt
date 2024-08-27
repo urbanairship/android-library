c89e96389fb49b00e880162653a3f9cffd4f656f
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-08-27 15:43:14 -0700

    Convert MessageTest to Kotlin

--- urbanairship-message-center/src/test/java/com/urbanairship/messagecenter/MessageTest.kt
+++ urbanairship-message-center/src/test/java/com/urbanairship/messagecenter/MessageTest.kt
@@ -1,77 +1,92 @@
 /* Copyright Airship and Contributors */
+package com.urbanairship.messagecenter
 
-package com.urbanairship.messagecenter;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.urbanairship.json.JsonException
+import com.urbanairship.json.JsonValue
+import com.urbanairship.util.DateUtils
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
+import org.intellij.lang.annotations.Language
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import com.urbanairship.json.JsonException;
-import com.urbanairship.json.JsonValue;
-import com.urbanairship.util.DateUtils;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.Map;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertTrue;
-
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4::class)
 public class MessageTest {
 
-    private static final String MCRAP_MESSAGE = "{\"content_size\":44,\"extra\":{}," +
-            "\"message_url\":\"https:\\/\\/device-api.urbanairship.com\\/api\\/user\\/test\\/messages\\/message\\/MESSAGE_ID\\/\"," +
-            "\"title\":\"MESSAGE_TITLE\",\"message_sent\":\"2015-09-23 16:46:26\",\"options\":{\"asset_hosted\":\"true\"}," +
-            "\"message_id\":\"MESSAGE_ID\",\"message_body_url\":\"https:\\/\\/dl.urbanairship.com\\/binary\\/token\\/app\\/MESSAGE_ID\\/body\\/\"," +
-            "\"message_read_url\":\"https:\\/\\/device-api.urbanairship.com\\/api\\/user\\/test\\/messages\\/message\\/MESSAGE_ID\\/read\\/\"," +
-            "\"unread\":false,\"content_type\":\"text\\/html\", \"extra\": {\"some_key\": \"some_value\"}}";
-
-    /**
-     * Test message parses its data correctly.
-     */
+    /** Test message parses its data correctly. */
     @Test
-    public void testMessage() throws JsonException {
-        Message message = Message.create(JsonValue.parseString(MCRAP_MESSAGE), true, false);
-
-        assertEquals("MESSAGE_ID", message.getMessageId());
-        assertEquals("MESSAGE_TITLE", message.getTitle());
-        assertEquals("https://dl.urbanairship.com/binary/token/app/MESSAGE_ID/body/", message.getMessageBodyUrl());
-        assertEquals("https://device-api.urbanairship.com/api/user/test/messages/message/MESSAGE_ID/read/", message.getMessageReadUrl());
-        assertEquals("https://device-api.urbanairship.com/api/user/test/messages/message/MESSAGE_ID/", message.getMessageUrl());
-        assertEquals(1443026786000l, message.getSentDateMS());
-        assertFalse(message.isRead());
-        assertFalse(message.isDeleted());
+    @Throws(JsonException::class)
+    public fun testMessage() {
+        val message = Message.create(JsonValue.parseString(MCRAP_MESSAGE), true, false)
+        assertEquals("MESSAGE_ID", message!!.messageId)
+        assertEquals("MESSAGE_TITLE", message.title)
+        assertEquals(
+            "https://dl.urbanairship.com/binary/token/app/MESSAGE_ID/body/",
+            message.messageBodyUrl
+        )
+        assertEquals(
+            "https://device-api.urbanairship.com/api/user/test/messages/message/MESSAGE_ID/read/",
+            message.messageReadUrl
+        )
+        assertEquals(
+            "https://device-api.urbanairship.com/api/user/test/messages/message/MESSAGE_ID/",
+            message.messageUrl
+        )
+        assertEquals(1443026786000L, message.sentDateMS)
+        assertFalse(message.isRead)
+        assertFalse(message.isDeleted)
 
         // Extras
-        assertEquals(1, message.getExtras().size());
-        assertEquals("some_value", message.getExtras().getString("some_key"));
-        assertEquals("some_value", message.getExtrasMap().get("some_key"));
+        assertEquals(1, message.extras.size())
+        assertEquals("some_value", message.extras.getString("some_key"))
+        assertEquals("some_value", message.extrasMap["some_key"])
 
         // Expiry
-        assertNull(message.getExpirationDate());
-        assertNull(message.getExpirationDateMS());
-        assertFalse(message.isExpired());
+        assertNull(message.expirationDate)
+        assertNull(message.expirationDateMS)
+        assertFalse(message.isExpired)
 
         // Raw message JSON
-        assertEquals(JsonValue.parseString(MCRAP_MESSAGE), message.getRawMessageJson());
+        assertEquals(JsonValue.parseString(MCRAP_MESSAGE), message.rawMessageJson)
     }
 
-    /**
-     * Test message parses its data correctly.
-     */
+    /** Test message parses its data correctly. */
     @Test
-    public void testMessageExpiry() throws JsonException {
+    @Throws(JsonException::class)
+    public fun testMessageExpiry() {
         // Add expiry
-        Map<String, JsonValue> map = JsonValue.parseString(MCRAP_MESSAGE).getMap().getMap();
-        map.put("message_expiry", JsonValue.wrap(DateUtils.createIso8601TimeStamp(10000l)));
-
-        Message message = Message.create(JsonValue.wrap(map), true, false);
+        val map = JsonValue.parseString(MCRAP_MESSAGE).requireMap().map
+        map["message_expiry"] = JsonValue.wrap(DateUtils.createIso8601TimeStamp(10000L))
+        val message = Message.create(JsonValue.wrap(map), true, false)
 
         // Expiry
-        assertEquals(10000l, message.getExpirationDateMS().longValue());
-        assertTrue(message.isExpired());
+        assertEquals(10000L, message!!.expirationDateMS)
+        assertTrue(message.isExpired)
     }
 
+    private companion object {
+        @Language("JSON")
+        private val MCRAP_MESSAGE = """
+        {
+            "content_size": 44,
+            "message_url": "https://device-api.urbanairship.com/api/user/test/messages/message/MESSAGE_ID/",
+            "title": "MESSAGE_TITLE",
+            "message_sent": "2015-09-23 16:46:26",
+            "options": {
+              "asset_hosted": "true"
+            },
+            "message_id": "MESSAGE_ID",
+            "message_body_url": "https://dl.urbanairship.com/binary/token/app/MESSAGE_ID/body/",
+            "message_read_url": "https://device-api.urbanairship.com/api/user/test/messages/message/MESSAGE_ID/read/",
+            "unread": false,
+            "content_type": "text/html",
+            "extra": {
+                "some_key": "some_value"
+            }
+        }
+        """.trimIndent()
+    }
 }
