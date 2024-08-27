c7f9bdd82c7d3e4041aa3b16ea37147b0f37b379
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-08-27 16:15:41 -0700

    Convert MessageCenterTestUtils to Kotlin

--- urbanairship-message-center/src/test/java/com/urbanairship/messagecenter/MessageCenterTestUtils.kt
+++ urbanairship-message-center/src/test/java/com/urbanairship/messagecenter/MessageCenterTestUtils.kt
@@ -1,61 +1,67 @@
 /* Copyright Airship and Contributors */
+package com.urbanairship.messagecenter
 
-package com.urbanairship.messagecenter;
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.urbanairship.json.JsonValue
+import com.urbanairship.messagecenter.Message.EXTRA_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_BODY_URL_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_EXPIRY_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_ID_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_READ_URL_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_REPORTING_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_SENT_KEY
+import com.urbanairship.messagecenter.Message.MESSAGE_URL_KEY
+import com.urbanairship.messagecenter.Message.TITLE_KEY
+import com.urbanairship.messagecenter.Message.UNREAD_KEY
+import com.urbanairship.messagecenter.Message.create
+import com.urbanairship.util.DateUtils
 
-import com.urbanairship.json.JsonValue;
-import com.urbanairship.util.DateUtils;
+public object MessageCenterTestUtils {
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import androidx.test.core.app.ApplicationProvider;
-
-public class MessageCenterTestUtils {
-
-    private static List<MessageEntity> messageEntities;
-    private static MessageDao messageDao;
-    private static MessageDatabase messageDatabase;
-
-    public static void setup() {
-        messageDatabase = MessageDatabase.createInMemoryDatabase(ApplicationProvider.getApplicationContext());
-        messageDao = messageDatabase.getDao();
+    private var messageEntities: List<MessageEntity>? = null
+    private lateinit var messageDao: MessageDao
+    private lateinit var messageDatabase: MessageDatabase
+    public fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        messageDatabase = MessageDatabase.createInMemoryDatabase(context)
+        messageDao = messageDatabase.dao
     }
 
-    public static void insertMessage(String messageId) {
-        insertMessage(messageId,null);
+    @JvmOverloads
+    public fun insertMessage(
+        messageId: String?,
+        extras: Map<String, String>? = null,
+        expired: Boolean = false
+    ) {
+        val message = createMessage(messageId, extras, expired)
+        val entity = requireNotNull(MessageEntity.createMessageFromPayload(messageId, message.rawMessageJson))
+        messageDao.insert(entity)
+        messageEntities = messageDao.messages
     }
 
-    public static void insertMessage(String messageId, Map<String, String> extras) {
-        insertMessage(messageId, extras, false);
-    }
-
-    public static void insertMessage(String messageId, Map<String, String> extras, boolean expired) {
-        Message message = createMessage(messageId, extras, expired);
-        messageDao.insert(MessageEntity.createMessageFromPayload(messageId, message.getRawMessageJson()));
-        messageEntities = messageDao.getMessages();
-    }
-
-    public static Message createMessage(String messageId, Map<String, String> extras, boolean expired) {
-        Map<String, Object> payload = new HashMap<>();
-        payload.put(Message.MESSAGE_ID_KEY, messageId);
-        payload.put(Message.MESSAGE_REPORTING_KEY, JsonValue.wrap(messageId));
-        payload.put(Message.MESSAGE_BODY_URL_KEY, String.format("https://go.urbanairship.com/api/user/tests/messages/%s/body/", messageId));
-        payload.put(Message.MESSAGE_READ_URL_KEY, String.format("https://go.urbanairship.com/api/user/tests/messages/%s/read/", messageId));
-        payload.put(Message.MESSAGE_URL_KEY, String.format("https://go.urbanairship.com/api/user/tests/messages/%s", messageId));
-        payload.put(Message.TITLE_KEY, String.format("%s title", messageId));
-        payload.put(Message.UNREAD_KEY, true);
-        payload.put(Message.MESSAGE_SENT_KEY, DateUtils.createIso8601TimeStamp(System.currentTimeMillis()));
-
+    public fun createMessage(
+        messageId: String?,
+        extras: Map<String, String>?,
+        expired: Boolean
+    ): Message {
+        val payload: MutableMap<String, Any?> = mutableMapOf(
+            MESSAGE_ID_KEY to messageId,
+            MESSAGE_REPORTING_KEY to JsonValue.wrap(messageId),
+            MESSAGE_BODY_URL_KEY to "https://go.urbanairship.com/api/user/tests/messages/$messageId/body/",
+            MESSAGE_READ_URL_KEY to "https://go.urbanairship.com/api/user/tests/messages/$messageId/read/",
+            MESSAGE_URL_KEY to "https://go.urbanairship.com/api/user/tests/messages/$messageId",
+            TITLE_KEY to "$messageId title",
+            UNREAD_KEY to true,
+            MESSAGE_SENT_KEY to DateUtils.createIso8601TimeStamp(System.currentTimeMillis())
+        )
         if (extras != null) {
-            payload.put(Message.EXTRA_KEY, extras);
+            payload[EXTRA_KEY] = extras
         }
-
         if (expired) {
-            payload.put(Message.MESSAGE_EXPIRY_KEY, DateUtils.createIso8601TimeStamp(0));
+            payload[MESSAGE_EXPIRY_KEY] = DateUtils.createIso8601TimeStamp(0)
         }
 
-        return Message.create(JsonValue.wrapOpt(payload), true, false);
+        return requireNotNull(create(JsonValue.wrapOpt(payload), true, false))
     }
-
 }
