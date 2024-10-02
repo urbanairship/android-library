94802eaf39b801c06dc4a5eb2f4b4423b7f5ff61
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2024-10-02 13:48:42 -0700

    Handle newlines in markdown formatted text properly and other improvements

--- urbanairship-layout/src/main/java/com/urbanairship/android/layout/util/StringExtensions.kt
+++ urbanairship-layout/src/main/java/com/urbanairship/android/layout/util/StringExtensions.kt
@@ -1,5 +1,7 @@
 package com.urbanairship.android.layout.util
 
+import androidx.core.text.htmlEncode
+
 internal fun String?.ifNotEmpty(block: (String) -> Unit) {
     if (!this.isNullOrEmpty()) {
         block(this)
@@ -25,30 +27,46 @@ private val linkRegex = "\\[(.*?)\\]\\((.*?)\\)".toRegex()
 private const val linkTag = """<a href="$2">$1</a>"""
 
 /** Matches `***bold + italic***` or `___bold + italic___` */
-private val boldAndItalicRegex = "[*_][*_][*_](.*?)[*_][*_][*_]".toRegex()
+private val boldAndItalicRegexStar = "\\*\\*\\*(.*?)\\*\\*\\*".toRegex()
+private val boldAndItalicRegexBar = "___(.*?)___".toRegex()
 private const val boldAndItalicTag = """<b><i>$1</i></b>"""
 
 /** Matches `**bold**` or `__bold__` */
-private val boldRegex = "[*_][*_](.*?)[*_][*_]".toRegex()
+private val boldRegexStar = "\\*\\*(.*?)\\*\\*".toRegex()
+private val boldRegexBar = "__(.*?)__".toRegex()
 private const val boldTag = """<b>$1</b>"""
 
 /** Matches `*italic*` or `_italic_` */
-private val italicRegex = "[*_](.*?)[*_]".toRegex()
+private val italicRegexStar = "\\*(.*?)\\*".toRegex()
+private val italicRegexBar = "_(.*?)_".toRegex()
 private const val italicTag = """<i>$1</i>"""
 
 /** Matches `~~strikethrough~~` */
 private val strikethroughRegex = "~~(.*?)~~".toRegex()
 private const val strikethroughTag = """<s>$1</s>"""
 
+/** Newlines `\n` */
+private const val newlineChar = "\\n"
+private const val newlineTag = "<br>"
+
 private fun basicMarkdownToHtml(markdown: String): String =
     markdown
+        // Encode any HTML characters in the original input so that we don't lose
+        // symbols like < or & after converting to HTML and rendering the text.
+        // This must be done first, before any other formatting is applied.
+        .htmlEncode()
+        // Replace newlines with <br>
+        .replace(newlineChar, newlineTag)
         // Replace [link](url) with <a href="url">link</a>
         .replace(linkRegex, linkTag)
         // Replace ***bold + italic*** and ___bold + italic___ with <b><i>bold + italic</i></b>
-        .replace(boldAndItalicRegex, boldAndItalicTag)
+        .replace(boldAndItalicRegexStar, boldAndItalicTag)
+        .replace(boldAndItalicRegexBar, boldAndItalicTag)
         // Replace **bold** and __bold__ with <b>bold</b>
-        .replace(boldRegex, boldTag)
+        .replace(boldRegexStar, boldTag)
+        .replace(boldRegexBar, boldTag)
         // Replace *italic* and _italic_ with <i>italic</i>
-        .replace(italicRegex, italicTag)
+        .replace(italicRegexStar, italicTag)
+        .replace(italicRegexBar, italicTag)
         // Replace ~~strikethrough~~ with <s>strikethrough</s>
         .replace(strikethroughRegex, strikethroughTag)
