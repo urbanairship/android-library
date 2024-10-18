3eb5c5b99a396d6b132f7babe0c29695f301c268
Igor Litvinenko <igor.litvinenko@airship.com>
2024-10-18 19:45:14 +0300

    wip

--- urbanairship-message-center/src/main/java/com/urbanairship/messagecenter/ui/MessageListFragment.kt
+++ urbanairship-message-center/src/main/java/com/urbanairship/messagecenter/ui/MessageListFragment.kt
@@ -1,19 +1,38 @@
 package com.urbanairship.messagecenter.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.urbanairship.Predicate
 import com.urbanairship.messagecenter.Message
+import com.urbanairship.messagecenter.R
+import com.urbanairship.messagecenter.ui.view.MessageListView
+import com.urbanairship.messagecenter.ui.view.MessageListView.OnShowMessageListener
 
 /** `Fragment` that displays the Message Center list and message view. */
-// TODO(m3-inbox): Make me work!
-public class MessageListFragment: Fragment() {
+public open class MessageListFragment: Fragment(R.layout.ua_fragment_message_list) {
 
+    private lateinit var messageListView: MessageListView
 
-    public fun setPredicate(predicate: Predicate<Message>) {
-        // TODO(m3-inbox): Implement me!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        messageListView = view.findViewById(R.id.message_list)
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    public fun setCurrentMessage(messageId: String) {
-        // TODO(m3-inbox): Implement me!
+    public fun setPredicate(predicate: Predicate<Message>) {
+        messageListView.predicate = predicate
+    }
+
+    public var isEditing: Boolean
+        set(value) { messageListView.isEditing = value }
+        get() = messageListView.isEditing
+
+    public var onShowMessageListener: OnShowMessageListener?
+        set(value) { messageListView.onShowMessageListener = value }
+        get() = messageListView.onShowMessageListener
+
+
+    public fun setHighlightedMessage(messageId: String) {
+        messageListView.setHighlightedMessage(messageId)
     }
 }
