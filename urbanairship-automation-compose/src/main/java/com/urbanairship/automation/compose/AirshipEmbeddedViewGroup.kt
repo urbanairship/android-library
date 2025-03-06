dde493c0f11259497fd23ba191dfe848eb16bf4c
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2025-03-06 14:00:39 -0800

    Refactor AirshipEmbeddedViewGroup to expose hoistable state (#1659)

--- urbanairship-automation-compose/src/main/java/com/urbanairship/automation/compose/AirshipEmbeddedViewGroup.kt
+++ urbanairship-automation-compose/src/main/java/com/urbanairship/automation/compose/AirshipEmbeddedViewGroup.kt
@@ -8,21 +8,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.urbanairship.android.layout.AirshipEmbeddedViewManager
 import com.urbanairship.android.layout.EmbeddedDisplayRequest
 import com.urbanairship.android.layout.ui.EmbeddedLayout
 import com.urbanairship.embedded.AirshipEmbeddedInfo
 import com.urbanairship.embedded.EmbeddedViewManager
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 /**
  * A container that allows all embedded content for the given [embeddedId]
@@ -40,56 +49,37 @@ public fun AirshipEmbeddedViewGroup(
     comparator: Comparator<AirshipEmbeddedInfo>? = null,
     content: @Composable BoxScope.(embeddedViews: List<EmbeddedViewItem>) -> Unit
 ) {
-    val scope = rememberCoroutineScope()
-
-    val displayRequests = EmbeddedViewManager.displayRequests(embeddedId, comparator, scope)
-        .map { it.list }
-        .distinctUntilChanged()
-        .collectAsStateWithLifecycle(emptyList())
-
-    val items: State<List<EmbeddedViewItem>> = derivedStateOf(policy = structuralEqualityPolicy()) {
-        displayRequests.value.map { request -> EmbeddedViewItem(request = request) }
-    }
-
-    Box(modifier) {
-        content(items.value)
-    }
+    AirshipEmbeddedViewGroup(
+        modifier = modifier,
+        state = rememberAirshipEmbeddedViewGroupState(embeddedId, comparator),
+        content = content
+    )
 }
 
 /**
- * An embedded view item, containing the [AirshipEmbeddedInfo] and the content to display.
+ * A container that allows all embedded content for the `embeddedId` defined on the given
+ * [AirshipEmbeddedViewGroupState] instance.
+ *
+ * This composable may be useful when access to the embedded view group state is needed outside of
+ * the `AirshipEmbeddedViewGroup` composable, embedded view group state should be hoisted, and for
+ * advanced custom logic that depends on the availability of embedded view content.
+ *
+ * When included inside of a lazy composable or scrolling list, state should be hoisted up, above
+ * the lazy composable or scrolling list, in order to ensure that the embedded view group state is
+ * maintained across recompositions.
+ *
+ * @param state The [AirshipEmbeddedViewGroupState] to be used by this embedded view group.
+ * @param modifier The modifier to be applied to the layout.
+ * @param content The `Composable` that will display the list of embedded view content.
  */
-@Immutable
-public data class EmbeddedViewItem internal constructor(
-    private val request: EmbeddedDisplayRequest
+@Composable
+public fun AirshipEmbeddedViewGroup(
+    state: AirshipEmbeddedViewGroupState,
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.(embeddedViews: List<EmbeddedViewItem>) -> Unit
 ) {
-    /** The [AirshipEmbeddedInfo] for this embedded content. */
-    public val info: AirshipEmbeddedInfo = AirshipEmbeddedInfo(
-        embeddedId = request.embeddedViewId,
-        instanceId = request.viewInstanceId,
-        priority = request.priority,
-        extras = request.extras,
-    )
-
-    /** The content to display for this embedded view item. */
-    @Composable
-    public fun content() {
-        val layout = EmbeddedLayout(
-            context = LocalContext.current,
-            embeddedViewId = request.embeddedViewId,
-            viewInstanceId = request.viewInstanceId,
-            args = request.displayArgsProvider.invoke(),
-            embeddedViewManager = EmbeddedViewManager
-        )
-
-        EmbeddedViewWrapper(
-            embeddedId = request.embeddedViewId,
-            embeddedLayout = layout,
-            embeddedSize = layout.getPlacement()?.size?.toEmbeddedSize(),
-            // Consumers provide their own placeholder, if desired.
-            placeholder = null,
-            modifier = Modifier.fillMaxWidth()
-        )
+    Box(modifier) {
+        content(state.items.value)
     }
 }
 
