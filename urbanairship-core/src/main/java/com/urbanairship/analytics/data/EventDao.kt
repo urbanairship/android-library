908d5127a54bed6a08d6b39a56f860a53b42604e
Apekka <antoine.pery-kasza@airship.com>
2025-09-04 14:37:25 +0200

    add internal qualifiers for EventDao

--- urbanairship-core/src/main/java/com/urbanairship/analytics/data/EventDao.kt
+++ urbanairship-core/src/main/java/com/urbanairship/analytics/data/EventDao.kt
@@ -21,46 +21,46 @@ import com.urbanairship.util.UAStringUtil
 internal abstract class EventDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract fun insert(event: EventEntity)
+    internal abstract fun insert(event: EventEntity)
 
     @Transaction
     @Query("SELECT * FROM events ORDER BY id ASC")
-    abstract fun get(): List<EventEntity>
+    internal abstract fun get(): List<EventEntity>
 
     @Transaction
     @Query("SELECT id, eventId, data FROM events ORDER BY id ASC LIMIT :limit")
-    abstract fun getBatch(limit: Int): List<EventIdAndData>
+    internal abstract fun getBatch(limit: Int): List<EventIdAndData>
 
     @Transaction
-    open fun deleteBatch(events: List<EventIdAndData>) {
+    internal open fun deleteBatch(events: List<EventIdAndData>) {
         for (event in events) {
             delete(event.eventId)
         }
     }
 
     @Query("DELETE FROM events WHERE eventId = :eventId")
-    abstract fun delete(eventId: String)
+    internal abstract fun delete(eventId: String)
 
     @Delete
-    abstract fun delete(vararg events: EventEntity)
+    internal abstract fun delete(vararg events: EventEntity)
 
     @Query("DELETE FROM events")
-    abstract fun deleteAll()
+    internal abstract fun deleteAll()
 
     @Query("SELECT COUNT(*) FROM events")
-    abstract fun count(): Int
+    internal abstract fun count(): Int
 
     @Query("SELECT SUM(eventSize) FROM events")
-    abstract fun databaseSize(): Int
+    internal abstract fun databaseSize(): Int
 
     @Query("SELECT sessionId FROM events ORDER BY id ASC LIMIT 1")
-    abstract fun oldestSessionId(): String?
+    internal abstract fun oldestSessionId(): String?
 
     @Query("DELETE FROM events WHERE sessionId = :sessionId")
-    abstract fun deleteSession(sessionId: String): Int
+    internal abstract fun deleteSession(sessionId: String): Int
 
     @Transaction
-    open fun trimDatabase(maxDatabaseSize: Int) {
+    internal open fun trimDatabase(maxDatabaseSize: Int) {
         while (databaseSize() > maxDatabaseSize) {
             val sessionId = oldestSessionId() ?: return
 
