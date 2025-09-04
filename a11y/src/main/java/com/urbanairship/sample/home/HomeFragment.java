20818a718332c6b49045e11e6a41052b0a2b09ba
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2025-09-04 13:56:54 -0700

    wire up parent height provider for a11y app embedded view (#1826)

--- a11y/src/main/java/com/urbanairship/sample/home/HomeFragment.java
+++ a11y/src/main/java/com/urbanairship/sample/home/HomeFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.urbanairship.UAirship;
 
 import com.urbanairship.analytics.CustomEvent;
 import com.urbanairship.sample.R;
@@ -33,6 +32,9 @@ public class HomeFragment extends Fragment {
         FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, container, false);
         bindThomasButtons(binding);
         bindIAAButtons(binding);
+
+        // Wire up embedded view to use the parent height of the scroll view.
+        binding.embeddedViewContainer.setParentHeightProvider(binding.scrollView::getHeight);
 
         return binding.getRoot();
     }
