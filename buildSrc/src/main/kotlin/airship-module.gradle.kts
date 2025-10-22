081a9d9b8beecd99039db62ea8ceec471c9052aa
ilitvinenko <mobexire6@gmail.com>
2025-10-22 17:52:23 +0200

    versions bump (#1878)
    * versions bump
    
    * updated java\kotlin version to 11, updated mockito-inline
    
    * fixed tests
    
    * bumped devapp java version
    
    ---------
    
    Co-authored-by: Ihor Lytvynenko <igor.litvinenko@airship.com>

--- buildSrc/src/main/kotlin/airship-module.gradle.kts
+++ buildSrc/src/main/kotlin/airship-module.gradle.kts
@@ -58,8 +58,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     lint {
@@ -72,7 +72,7 @@ kotlin {
     explicitApi = ExplicitApiMode.Warning
 
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_1_8
+        jvmTarget = JvmTarget.JVM_11
         jvmDefault = JvmDefaultMode.ENABLE
     }
 }
