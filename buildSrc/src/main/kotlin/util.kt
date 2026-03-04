dc98eb17348f0fee8f9642a4af07ac79836596ee
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2026-03-03 16:04:28 -0800

    Rework publishing (#1992)

--- buildSrc/src/main/kotlin/util.kt
+++
@@ -1,9 +0,0 @@
-
-import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.provideDelegate
-
-internal fun Project.prop(name: String) = findProperty(name)?.toString()
-
-internal fun env(name: String) = System.getenv(name)
